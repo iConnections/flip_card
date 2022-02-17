8d44e8de626db73f7164057566ba829083c6e2cf
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2022-02-16 20:37:48 -0500

    Update flip_card.dart

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -29,7 +29,7 @@ class AnimationCard extends StatelessWidget {
       animation: animation!,
       builder: (BuildContext context, Widget? child) {
         var transform = Matrix4.identity();
-        transform.setEntry(3, 2, 0.001);
+        transform.setEntry(3, 2, 0.000);
         if (direction == FlipDirection.VERTICAL) {
           transform.rotateX(animation!.value);
         } else {
